d3d7a840636c9f3dab4afe501742b88625383774
Madina <sagdeeva.m.r@mail.ru>
2022-10-05 22:46:09 +0300

    Zadacha 4 - Задача на нахождение максимального числа

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,32 @@
+﻿/*4. Напишите программу, которая принимает на вход три числа и выдаёт 
+максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите первое число:");
+string firstNumber = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string secondNumber = Console.ReadLine();
+Console.WriteLine("Введите третье число:");
+string thirdNumber = Console.ReadLine();
+
+int firstNum = int.Parse(firstNumber);
+int secondNum = int.Parse(secondNumber);
+int thirdNum = int.Parse(thirdNumber);
+int maxNumber = firstNum;
+
+if (firstNum > secondNum && firstNum > thirdNum)
+
+    maxNumber = firstNum;
+
+else if (secondNum > firstNum && secondNum > thirdNum)
+
+    maxNumber = secondNum;
+
+else 
+
+maxNumber = thirdNum;
+
+Console.WriteLine($"Число {maxNumber} является максимальным");
