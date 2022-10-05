2ccc52913c786f1843f3639e1f64cc129c2776a6
Madina <sagdeeva.m.r@mail.ru>
2022-10-05 22:48:13 +0300

    Zadacha 8 - Задача на показ всех чётных чисел

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,13 @@
+﻿/*8. Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число N:");
+int Number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < Number; i++)
+{
+    Console.Write($"{i}");
+}
